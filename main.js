1241a3920e1c81a62ff9f80c3ff31d2f645ae0a6
Ainhoa Martinez <amartineziribas@gmail.com>
2019-10-27 11:50:30 -0700

    sunday fixes

--- main.js
+++ main.js
@@ -16,6 +16,44 @@ function shuffle(deck) {
   return newArray
 }
 
+// function handleGame() {
+//   let arrCards = []
+
+//   // Handling cards
+//   $("#buttons").on("click", "button", function(e) {
+//     e.preventDefault()
+
+//     // putting values of clicked cards into the array
+//     arrCards.push($(this))
+
+//     //if 2 cards are active, disables you from selecting other cards
+//     if (arrCards.length <= 2) {
+//       $(this).toggleClass("face-up")
+//     }
+
+//     if (arrCards.length === 2) {
+//       // if total cards in array are equal to 2 then compare win/lose scenario
+//       if (arrCards[0].html() != arrCards[1].html()) {
+//         setTimeout(function() {
+//           arrCards[0].toggleClass("face-up")
+//           arrCards[1].toggleClass("face-up")
+//           arrCards = []
+//         }, 1200)
+//         // boo.play()
+//       } else {
+//         arrCards[0].addClass("stay")
+//         arrCards[1].addClass("stay")
+
+//         if (arrCards[0].hasClass("stay") && arrCards[1].hasClass("stay")) {
+//           $(".stay").attr("disabled", true)
+//           win.play()
+//         }
+//         arrCards = []
+//       }
+//     }
+//   })
+// }
+
 function readyDeck(deck) {
   let buttons = ""
   deck = shuffle(deck.split(""))
@@ -26,64 +64,183 @@ function readyDeck(deck) {
   //   deck = shuffle(deck)
   $("#buttons").html(buttons)
 }
+function resetGame() {
+  document.location.reload(true)
+}
+
+function timer() {
+  timerRunning = true
+
+ counter= setInterval(function() {
+    secs=secs+1
+    
+    $("#timer").html("Time: " + getTimeStr(secs))
+  }, 1000)
+}
+
+
+function handleGame() {
+  let arrCards = []
+  // Handling cards
+  $("#buttons").on("click", "button", function(e) {
+    e.preventDefault()
+    if (!timerRunning) {
+      timer()
+      
+    }
+    // putting values of clicked cards into the array
+    //if 2 cards are active, disables you from selecting other cards
+    if (!$(this).hasClass("stay")) {
+      
+      arrCards.push($(this))
+    }
+    if (
+      arrCards.length <= 2 &&
+      !$(this).hasClass("face-up") &&
+      !$(this).hasClass("stay")
+    ) {
+      $(this)
+        .not(".stay")
+        .toggleClass("face-up")
+    }
+    if (arrCards.length === 2) {
+      turn = turn + 1
+      $(".turns").html(`Turns:${turn}`)
+
+      // if total cards in array are equal to 2 then compare win/lose scenario
+      if (arrCards[0].html() != arrCards[1].html()) {
+        setTimeout(function() {
+          arrCards[0].toggleClass("face-up")
+          arrCards[1].toggleClass("face-up")
+          arrCards = []
+        }, 1200)
+        // boo.play()
+      } else {
+        pairs = pairs + 1
+        arrCards[0].addClass("stay")
+        arrCards[1].addClass("stay")
+        win.play()
+
+        // if (arrCards[0].hasClass("stay") && arrCards[1].hasClass("stay")) {
+        //   $(".stay").attr("disabled", true)
+        // }
+        arrCards = []
+        if (pairs === 2) {
+          //change to 9!
+          clearInterval(counter) //to stop interval
+          turnsScore()
+          timerscore()
+          winnerScreen()
+        }
+      }
+    }
+  })
+}
+
+
+function getTimeStr(sec) {
+  var minutes = sec / 60
+  var seconds = Math.floor((minutes - Math.floor(minutes)) * 60)
+  return (
+    "0" +  Math.floor(minutes) +
+    ":" +
+    (seconds < 10 ? "0" + seconds.toFixed(0) : seconds.toFixed(0))
+  )
+}
+
+function finalscreen(){
+  if (result === 9 || result === 10){
+    outcome="Amazing"
+   } else if (result === 7 || result === 8){
+    outcome="Great Job"
+   } else if (result === 5 || result === 6){
+    outcome="Not Bad"
+   } else if (result === 3 || result === 4){
+    outcome="Pretty Bad"
+   } else if (result < 3){
+    outcome="You Suck!"
+   }
+}
+
+function winnerScreen() {
+  result = turnbonus + timerbonus
+ 
+  finalscreen()
+  
+  var finalpage = `<p class="finalpage">
+                   <p> It took you ${secs} seconds.</p>
+                   <p> It to you ${turn} turns </p>
+                   <p> Your outcome is: ${outcome}</p>
+  `
+  $(".winnerScreen").html(finalpage)
+  
+  
+
+  $("#fireworks").show()
+  
+   $(".winnerScreen").show()
+
+
+}
+
+function turnsScore() {
+
+if(turn>=0 && turn<=15)
+{
+    turnbonus=10
+} else if(turn>=25){
+  turnbonus=0
+}
+
+switch (turn)
+{
+  case 16:
+    turnbonus=9;
+    break
+  case 17:
+    turnbonus=8;
+    break
+  case 18:
+    turnbonus=7;
+    break
+  case 19:
+    turnbonus=6;
+    break
+  case 20:
+    turnbonus=5;
+    break
+  case 21:
+    turnbonus=4;
+    break
+  case 22:
+    turnbonus=3;
+    break
+  case 23:
+    turnbonus=2;
+    break
+  case 24:
+    turnbonus=1;
+    break
+                
+}
+
+}
+
+function timerscore() {
+  let howmanysecs =secs 
+  if (howmanysecs < 60) {
+    timerbonus = 3
+  } else if (howmanysecs >= 60 && howmanysecs < 120) {
+    timerbonus = 2
+  } else if (howmanysecs >= 120 && howmanysecs < 180) {
+    timerbonus = 1
+  } else {
+    timerbonus = 0
+  }
+  
+  }
 
 $(document).ready(function() {
   readyDeck(deck)
-  let arrCards=[]
-  // let count=0;
-    // setTimeout()
-    // setTimeout(function(){ alert("Hello"); }, 3000);
-
-    
-  $("#buttons").on("click", "button", function(e) {
-    e.preventDefault(
-    )
-     arrCards.push($(this))
-     console.log(arrCards)
-    $(this).toggleClass("face-up")
-    if(arrCards.length==2)
-    {
-      if(arrCards[0][0].innerHTML !=arrCards[1][0].innerHTML)
-      {
-        setTimeout(function(){
-          arrCards[0].toggleClass("face-up")
-          arrCards[1].toggleClass("face-up")
-          arrCards=[];
-        },3000)
-        
-       
-        
-      }else
-      {
-
-        // alert("same")
-      }
-      
-    }
-
-    
-    // if(count==1)
-    // {
-    //   // alert(count)
-
-
-    //     card2=$(this)  
-        
-    // }else{
-      // count = 0
-    // }if(card1!=card2){
-      // alert("different")
-      
-    //   // card1.toggleClass("face-down")
-    //   card2.toggleClass("face-down")
-
-
-    // }else{
-    //   //same cards
-    // }
-
-
-  })
-
-
+  handleGame()
 })
--- variables.js
+++ variables.js
@@ -1,1 +1,16 @@
 let deck = "abcdefghiabcdefghi"
+var boo = new Audio("./boo.mp3")
+var win = new Audio("./Applause.mp3")
+var flips = [boo, win]
+var resetButton
+let counter; //to stop the interval
+let counter2; //to stop the interval
+let secs=0
+let turn=0
+let pairs=0
+let timerbonus=0
+let turnbonus=0
+let timerRunning=false
+var startTime = new Date()
+var result = timerbonus + turnbonus
+var outcome =""
